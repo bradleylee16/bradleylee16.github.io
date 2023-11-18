a5e82bb038cf21a29c2ebc27007c0bda1f492198
Brad <29055107+bradleylee16@users.noreply.github.com>
2023-11-18 04:25:32 -0500

    text updates

--- info.js
+++ info.js
@@ -1,39 +1,44 @@
 //INFO TEXT
+//
+//
+//  2023: forgive my old spaghetti code. I rewrote clock.js if you want to check out some more recent code. This I would probably rewrite in a modern framework some time
+//
+//
 var infoText = {
-    "i1a":"Click on buttons and tabs to navigate"
-    ,"i1b":"Commands can also be typed in the terminal to navigate"
-    ,"i1c":"Any text surrounded by [brackets] can be typed as a command"
-    ,"i2a":"Press Enter to skip a typing animation"
-    ,"i2b":"Press the arrow up and down keys to cycle through recent commands"
-    ,"i2c":"Settings and history will save between visits"
-    ,"i3a":"[border 1] [border 0]"
-    ,"i3b":"Turns on/off element div borders"
-    ,"i3c":"[retype 1] [retype 0]"
-    ,"i3d":"Turns on/off repeating of the typing effect"
-    ,"i3e":"[history]"
-    ,"i3f":"Prints in-site terminal history to JS terminal"
+    "i1a":"Click on buttons and tabs to navigate",
+    "i1b":"Commands can also be typed in the terminal to navigate",
+    "i1c":"Any text surrounded by [brackets] can be typed as a command",
+    "i2a":"Press Enter to skip a typing animation",
+    "i2b":"Press the arrow up and down keys to cycle through recent commands",
+    "i2c":"Settings and history will save between visits",
+    "i3a":"[border 1] [border 0]",
+    "i3b":"Turns on/off element div borders",
+    "i3c":"[retype 1] [retype 0]",
+    "i3d":"Turns on/off repeating of the typing effect",
+    "i3e":"[history]",
+    "i3f":"Prints in-site terminal history to JS terminal",
 }
 var list1 = {
-    "i1a":"> Click on buttons and tabs to navigate"
-    ,"i1b":"> Commands can also be typed in the terminal to navigate"
-    ,"i1c":"> Any text surrounded by [brackets] can be typed as a command"
+    "i1a":"> Click on buttons and tabs to navigate",
+    "i1b":"> Commands can also be typed in the terminal to navigate",
+    "i1c":"> Any text surrounded by [brackets] can be typed as a command",
 }
 var list2 = {
-     "i2a":"> Press Enter to skip a typing animation"
-    ,"i2b":"> Press the arrow up and down keys to cycle through recent commands"
-    ,"i2c":"> Settings and history will save between visits"
-    ,"i2d":"> Colors and animations customizable through commands"
-    ,"i2e": "> Commands are non case sensitive"
+     "i2a":"> Press Enter to skip a typing animation",
+    "i2b":"> Press the arrow up and down keys to cycle through recent commands",
+    "i2c":"> Settings and history will save between visits",
+    "i2d":"> Colors and animations customizable through commands",
+    "i2e": "> Commands are non case sensitive"
 }
 var list3 = {
-     "i3a":"> [animation 1] [animation 0]: Changes the animation style for that page"
-    ,"i3b":"> [border 1] [border 0]: Turns on/off element div borders"
-    ,"i3c":"> [theme #000000] Set theme to green,blue,red,purple,classic, or hex value"
-    ,"i3d":"> [retype 1] [retype 0]: Turns on/off repeating of the typing effect"
-    ,"i3e":"> [printArgs 1] [printArgs 0] toggles printing cmd args to JS terminal"
-    ,"i3f":"> [history]: Prints in-site terminal history to JS terminal"
-    ,"i3g":"> [cookies]: Prints all cookies saved on the site"
-    ,"i3h":"> [reset]: Resets all settings to default"
+     "i3a":"> [animation 1] [animation 0]: Changes the animation style for that page",
+    "i3b":"> [border 1] [border 0]: Turns on/off element div borders",
+    "i3c":"> [theme #000000] Set theme to green,blue,red,purple,classic, or hex value",
+    "i3d":"> [retype 1] [retype 0]: Turns on/off repeating of the typing effect",
+    "i3e":"> [printArgs 1] [printArgs 0] toggles printing cmd args to JS terminal",
+    "i3f":"> [history]: Prints in-site terminal history to JS terminal",
+    "i3g":"> [cookies]: Prints all cookies saved on the site",
+    "i3h":"> [reset]: Resets all settings to default",
 }
 var listIDs = {"list1":list1,"list2":list2,"list3":list3};
 var idClicked = {"list1":false,"list2":false,"list3":false};
@@ -41,49 +46,46 @@ var idClicked = {"list1":false,"list2":false,"list3":false};
 var resumeText = {
     "r1a":"[Education]"
     ,"r1b":"University of Maryland: College Park"
-    ,"r1c":"B.S. Computer Science - Expected 2020"
-    ,"r1d":"GPA: 3.25"
+    ,"r1c":"B.S. Computer Science - 2016-2020"
     ,"r2a":"[Skills]"
     ,"r2b":"Java"
     ,"r2c":"Python"
     ,"r2d":"C"
     ,"r2e":"Javascript"
     ,"r2f":"HTML/CSS"
-    ,"r2g":"Ruby"
+    ,"r2g":"Angular"
     ,"r3a":"[Projects]"
     ,"r3b":"Personal Website"
-    ,"r3c":"Hub for personal projects and code"
+    ,"r3c":"Learned basics of HTML/CSS, and javascript"
     ,"r3d":"Terminal design with functioning commands"
-    ,"r3e":"Extensive use of Javascript"
     ,"r3f":"Text Clock"
     ,"r3g":"Clock comprised of text that lights up to show the current time"
     ,"r3h":"Additional tools and commands implemented"
-    ,"r3i":"Personal javascript introductory project"
+    ,"r3i":"Fun javascript introductory project"
     ,"r4a":"[Experience]"
-    ,"r4b":"IT Technician at University of Maryland"
+    ,"r4b":"Software Engineer: 2020-Present"
     /*,"r4c":"Sept 2017 - Ongoing"*/
-    ,"r4d":"Provided University professors and staff with direct software and network support"
-    ,"r4e":"Worked independently and collaboratively with coworkers, under loose supervision"
-    ,"r4f":"Learned new IT skills on the job from coworkers and supervisors"
-    ,"r4g":"Trained newly hired technicians"
-    ,"r4h":"Volunteer at Liberty Science Center, Jersey City, NJ"
+    ,"r4d":"Was an integral part of developing healthcare software used in hospitals and insurance"
+    ,"r4e":"Developed webapp to view, modify, import, and export data using Angular, Spring Boot REST API, and Oracle SQL"
+    ,"r4f":"Created and modified existing CI/CD Pipelines using Jenkins"
+    ,"r4g":"Wrote SQL generated reports requested by team members, as well as database integrity heuristics"
+    ,"r4h":"Deployed and administered internally used web server hosted on AWS EC2 Linux"
     /*,"r4i":"Aug 2014 - 2015"*/
-    ,"r4j":"Led educational activities"
-    ,"r4k":"Teaching assistant at summer camp"
-    ,"r4l":"Volunteered 260 hours of service"
+    ,"r4j":"Freelance Web Scraper: 2020, 2022, 2023"
+    ,"r4k":"Wrote web scrapers targeted at county websites on election nights for general and midterm elections"
+    ,"r4l":"Contributed to rapid nationwide counting of votes"
 };
 var rList1 = {
     "r1b":"University of Maryland: College Park"
-    ,"r1c":"B.S. Computer Science - Expected 2020"
-    ,"r1d":"GPA: 3.25"
+    ,"r1c":"B.S. Computer Science - Aug 2016- May 2020"
 }
 var rList2 = {
     "r2b":"Java"
     ,"r2c":"Python"
-    ,"r2d":"C"
+    ,"r2d":"SQL"
     ,"r2e":"Javascript"
     ,"r2f":"HTML/CSS"
-    ,"r2g":"Ruby"
+    ,"r2g":"Angular"
 }
 var rList3 = {
     "r3b":"Personal Website"
@@ -96,38 +98,37 @@ var rList3 = {
     ,"r3i":"Personal javascript introductory project"
 }
 var rList4 = {
-    "r4b":"IT Technician at University of Maryland"
+    "r4b":"Software Engineer at 3M Health Information Systems 2020-Present"
     /*,"r4c":"Sept 2017 - Ongoing"*/
-    ,"r4d":"Provided faculty and staff with direct software and network support"
-    ,"r4e":"Worked independently and collaboratively under loose supervision"
-    ,"r4f":"Learned skills on the job from peers and supervisors"
-    ,"r4g":"Trained newly hired technicians"
-    ,"r4h":"Volunteer at Liberty Science Center"
-    ,"r4i":"Jersey City, NJ - Aug 2014 - 2015"
-    ,"r4j":"Led educational activities"
-    ,"r4k":"Teaching assistant at summer camp"
-    ,"r4l":"Volunteered 260 hours of service"
+    ,"r4d":"Was an integral part of developing healthcare software used in hospitals and insurance"
+    ,"r4e":"Developed webapp to view, modify, import, and export data using Angular, Java and SQL"
+    ,"r4f":"Created and modified existing CI/CD Pipelines using Jenkins"
+    ,"r4g":"Wrote SQL generated reports requested by team members, and database integrity heuristics"
+    ,"r4h":"Deployed and administered internally used web server hosted on AWS EC2 Linux"
+    ,"r4i":"Freelance Web Scraper: 2020, 2022, 2023"
+    ,"r4j":"Wrote web scrapers targeted at county sites for for general and midterm election nights"
+    ,"r4k":"Contributed to rapid nationwide counting of votes"
 }
 var rListIDs = {"rList1":rList1,"rList2":rList2,"rList3":rList3,"rList4":rList4};
 var rIdClicked = {"rList1":false,"rList2":false,"rList3":false,"rList4":false};
 //MAIN TEXT
 var mList1 = {
-    "m1a": "> Brad Lee is from New Jersey. He made this website",
-    "m1b": "> Brad studies computer science at the University of Maryland",
+    "m1a": "> Brad Lee is from NJ, but now lives in Brooklyn. He made this website",
+    "m1b": "> Brad studied computer science at the University of Maryland",
     "m1c": "> He enjoys coding, movies, and TV"
 }
 var mList2 = {
     "m2a": "> [Text Clock]",
     "m2b": "> [Github Code]",
-    "m2c": "> Coming Soon!"
+    "m2c": "> Coming Soon(ish)!"
 }
 var mList3 = {
-    "m3a": "> bradleylee16@gmail.com",
+    "m3a": "> notarealemail@gmail.com",
     "m3b": "> [Linkedin]"
 }
 var mList4 = {
     "m4a": "> [Online] Version",
-    "m4b": "> [PDF] Version",
+    "m4b": "> [PDF] Version: If you're reading this, you probably already have it ",
 }
 var mListIDs = {"mList1":mList1,"mList2":mList2,"mList3":mList3,"mList4":mList4};
 var mIdClicked = {"mList1":false,"mList2":false,"mList3":false,"mList4":false};
